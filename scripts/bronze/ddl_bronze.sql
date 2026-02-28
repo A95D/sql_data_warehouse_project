6501812cfe0a39a1f9d55828610dbb99e266f278
A95D <gamebomb55@gmail.com>
2026-02-28 12:10:15 +0300

    Create SQL tables for customer info and sales revenue

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,15 @@
+create table if not exists crm_cust_info(
+  id int,
+  first_name varchar,
+  last_name varchar,
+  email varchar,
+  insertion_timestamp timestamp default now()
+);
+
+create table if not exists erp_sales_rev(
+  order_id int,
+  customer_id int,
+  order_date date,
+  amount numeric,
+  insertion_timestamp timestamp default now()
+)
